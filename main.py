a64ed13889c410f4ccc90fd83be0b9beabe6a9f8
arialu a <arialu@INACAP.CL>
2022-10-05 20:37:47 -0300

    Creando clases basicas

---
+++ main.py
@@ -0,0 +1,39 @@
+#clases
+
+class Animal():
+    def __init__(self,nombre) -> None:
+        self.nombre=nombre
+
+
+class Gato(Animal):
+    def __init__(self, nombre,cant_bigotes) -> None:
+        super().__init__(nombre)
+        self.cant_bigotes=cant_bigotes
+
+    def hablar(self):
+        print("Miau Miau Miau")
+
+class Perro(Animal):
+    def __init__(self, nombre,muerde) -> None:
+        super().__init__(nombre)
+        self.muerde=muerde
+
+    def hablar(self):
+        print("Guau Guau Guau")
+
+class Vaca(Animal):
+    def __init__(self, nombre,es_lechera) -> None:
+        super().__init__(nombre)
+        self.es_lechera=es_lechera
+    
+    def hablar(self):
+        print("Muuu Muuu Muuu")
+
+animal1=Gato("Michi",23)
+animal2=Perro("Firulai",True)
+animal3=Vaca("Lola",False)
+lista=[animal1,animal2,animal3]
+for i in lista:
+    i.hablar()
+
+
